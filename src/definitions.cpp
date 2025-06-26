0a6b91a2b64e9b2dbd0ad1329669104d680a58ae
alejandroayala3 <00237625@uca.edu.sv>
2025-06-25 23:39:34 -0600

    definitions

---
+++ src/definitions.cpp
@@ -0,0 +1,48 @@
+#include "header.h"
+
+void sleep(int time_ms)
+{
+}
+
+int load_Locations(vector<location> &locations, const string &file)
+{
+    return 0;
+}
+
+int load_Actions(vector<action> &actions, const string &file)
+{
+    return 0;
+}
+
+void initialize_player(player &player)
+{
+}
+
+void show_Scene(player &player, const vector<location> &locations)
+{
+}
+
+string handle_Entry()
+{
+    return string();
+}
+
+void process_Action(string &entry, player, vector<action> actions, const vector<location> &locations)
+{
+}
+
+void handle_hostile_environment(player &player)
+{
+}
+
+void discover_memory(player&player, string & memory)
+{
+}
+
+void show_credits()
+{
+}
+
+void final_game()
+{
+}
