d5e5867f40c86191a94d6e1403da782a3d29d4ac
alejandroayala3 <00237625@uca.edu.sv>
2025-06-29 21:12:00 -0600

    Actualizaciones a las definiciones

--- src/definitions.cpp
+++ src/definitions.cpp
@@ -4,12 +4,19 @@ void playGame()
 {
 }
 
-void sleep(int time_ms)
+void sleep(int time_seconds)
 {
-
+    time_seconds=900;
+    if (time_seconds >=900)
+    {
+        cout<< "Se te acabo el tiempo. El reator ha explotado." << endl;
+        cout << "=====GAME OVER=====" << endl;
+        
+    }
+    
 }
 
-int load_Locations(vector<location> &locations, const string &file)
+int load_Locations(vector<location> &locations, const string &file, player &player)
 {
     for (const auto &loc : locations)
     {
@@ -21,7 +28,7 @@ int load_Locations(vector<location> &locations, const string &file)
             {
                 cout<< "Este lugar es hostil!" << endl;
                 cout << "Ten cuidado!" << endl;
-                handle_hostile_environment(player);
+                final_game();
             }
             
         }return 0;
@@ -34,7 +41,7 @@ int load_Locations(vector<location> &locations, const string &file)
 
 int load_Actions(vector<action> &actions, const string &file)
 {
-    return 0;
+
 }
 
 void initialize_player(player &player)
@@ -52,25 +59,42 @@ void show_Scene(player &player, const vector<location> &locations)
 
 string handle_Entry()
 {
-    return string();
+    string input;
+    cout<< "Que accion vas a tomar?"<< endl;
+    getline(cin,input);
+
+    return string(input);
 }
 
-void process_Action(string &entry, player, vector<action> actions, const vector<location> &locations)
+void process_Action(string &entry, player player, vector<action> actions, const vector<location> &locations)
 {
+    for (auto &action :actions)
+    {
+        if (action.sourceLocation== player.currentLocation && action.entry==entry)
+        {
+            cout<< action.endMessage << endl;
+            player.currentLocation =  action.destinationLocation;
+            show_Scene(player, locations);
+            return;    
+        }
+        
+    }
+    cout << "No se puede hacer eso aqui." << endl;
 }
-
-void handle_hostile_environment(player &player)
-{
-}
-
 void discover_memory(player&player, string & memory)
 {
+    player.memoriesFound.push_back(memory);
+    cout << "Has encontrado un modulo de memoria: " << memory << endl;
 }
 
 void show_credits()
 {
+    cout << "==========CREDITOS============" << endl;
+    cout << "Dyrected by: Alejandro Ayala" << endl;
 }
 
 void final_game()
 {
+    cout << "    LLegaste al modulo final    " << endl;
+    cout << "Felicidades por completar el juego." << endl;
 }
--- src/header.h
+++ src/header.h
@@ -5,15 +5,13 @@
 
 using namespace std;
 
-struct player                                  //Represents all the data and current state of the player.
+struct player                                  
 {
-
-int lives;                                   //Player's remaining lives.
-int time;                                   //Time remaining in seconds.
-string currentLocation;                    //The 'id' of the location where the player is located.
-vector<string>inventory;                  //List of items the player has collected.
-string currentgoal;                      //The current objective shown to the player
-vector<string>memoriesFound;            //List of memory fragments the player has discovered.
+                                  
+int time;                                   //time remaining in seconds.
+string currentLocation;                    //the 'id' of the location where the player is located.                
+string currentgoal;                      //the current objective shown to the player
+vector<string>memoriesFound;            
 string  araState;                      // Saves the current status of the relationship with ARA (NEUTRAL, HELPFUL, HOSTILE).
 bool finalModuleFound;                //It becomes 'true' when the player finds the key item at the end.
 
@@ -24,33 +22,31 @@ bool finalModuleFound;                //It becomes 'true' when the player finds 
 
 struct location                          //Represents a place in the game world.
 {
-string description;                    //Location identifier
-string locations;                     //The text the player sees when at this location.
+string description;                    
+string locations;                    
 bool isHostile;                      //'true' if the location is dangerous
 };
 
 struct action                          //Represents a possible action or interaction in the game.
 {
-string sourceLocation;               //The 'id' of the location where you must be to perform the action.
-string entry;                       //The exact command the player must type.
-string destinationLocation;        //The 'id' of the location the player will move to if the action is successful.
-string endMessage;                //The message that is displayed when the action completes successfully.
-
+string sourceLocation;             
+string entry;                      //The exact command the player must type.
+string destinationLocation;        
+string endMessage;                
 };
 
-void playGame();//The main function that orchestrates the entire game loop.
-void sleep(int time_ms);//Pauses the execution of the program for a specified time.
-int load_Locations(vector<location>& locations, const string& file);//Read location data
-int load_Actions(vector<action>& actions, const string& file);//Reads action/puzzle data from a text file.
-void initialize_player(player & player);//Sets the player's initial values ​​at the start of a game.
-void show_Scene(player & player , const vector<location>& locations);//Displays the description of the player's current location and status.
-string handle_Entry();//Captures and returns the command the player types.
-void process_Action(string & entry, player, vector<action> actions,const vector<location>& locations);//Processes the player's command and updates the game state.
-bool verify_EndConditions(player);//Checks if the player has lost (no lives/time) or won.
-void handle_hostile_environment(player&player);//Manages what happens when the player is in a hostile zone.
-void discover_memory(player&player, string & memory);//Handles the event of finding a new memory fragment.
-void interact_with_Ara(player);//Manage dialogue and key decisions with the character ARA.
-void handle_final_choice(player);//Manages the sequence of end-game decisions.
-void show_final_ending(const player);//Displays the final text corresponding to the decision made.
-void show_credits();//Displays the game's ending credits.
-
+void playGame();                        //The main function that orchestrates the entire game loop.
+void sleep(int time_seconds);                            //Pauses the execution of the program for a specified time.
+int load_Locations(vector<location>& locations, const string& file, player &player);                //Read location data
+int load_Actions(vector<action>& actions, const string& file);          //Reads action/puzzle data from a text file.
+void initialize_player(player & player);                //Sets the player's initial values ​​at the start of a game.
+void show_Scene(player & player , const vector<location>& locations);               //Displays the description of the player's current location and status.
+string handle_Entry();                  //Captures and returns the command the player types.
+void process_Action(string & entry, player, vector<action> actions,const vector<location>& locations);        //Processes the player's command and updates the game state.
+bool verify_EndConditions(player);          //Checks if the player has lost or won.
+void discover_memory(player&player, string & memory);       //Handles the event of finding a new memory fragment.
+void interact_with_Ara(player);         //Manage dialogue and key decisions with the character ARA.
+void handle_final_choice(player);           //Manages the sequence of end-game decisions.
+void show_final_ending(const player);           //Displays the final text corresponding to the decision made.
+void show_credits();                    //Displays the game's ending credits.
+void final_game();                              //displays the final message after the credits
